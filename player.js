a44f62b4466f668cf127a1addd324d50c8438dd7
Felix De Montis <felix@demont.is>
2014-04-12 11:21:33 +0200

    V1.0.2

--- player.js
+++ player.js
@@ -1,11 +1,21 @@
 
 module.exports = {
 
-  VERSION: "V1.0.1",
+  VERSION: "V1.0.2",
 
   bet_request: function(game_state) {
+    
+    
     var gs = JSON.parse(game_state);
-    return gs.minimum_raise;
+    
+    if (gs.small_blind*3 <= gs.pot) {
+      return gs.minimum_raise;
+    } else {
+      return 0;
+    }
+    
+    
+    
   },
 
   showdown: function(game_state) {
